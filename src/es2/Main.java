c0b90f5eb04183516302ac9307c1bbd78aa8dc6b
Federico <brunetti.federico.13@gmail.com>
2025-06-24 18:16:43 +0200

    esercizio 2 completamente terminato

--- src/es2/Main.java
+++ src/es2/Main.java
@@ -1,6 +1,7 @@
 package es2;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Scanner;
 
@@ -18,11 +19,16 @@ public static ArrayList makeList() {
 
 
     }
+//    Collection.sort(numberList);
+    numberList.sort(null);
     return  numberList;
 }
 
 public static ArrayList invertList(ArrayList lista) {
     ArrayList <Integer> oppositeList = new ArrayList<>();
+    for (int i=0; i < lista.size()-1; i++){
+        oppositeList.add ((Integer) lista.get(i));
+    }
 for (int i=lista.size()-1; i>=0; i--) {
     oppositeList.add ((Integer) lista.get(i));
 
@@ -32,24 +38,42 @@ for (int i=lista.size()-1; i>=0; i--) {
 }
 
 
-public static  ArrayList halfList (ArrayList lista, boolean pari){
-    if (pari){
+public static  void  halfList (ArrayList lista, boolean pari){
+    if (!pari){
+      for (int i =0; i< lista.size(); i = i+2) {
+          System.out.println(lista.get(i));
+      }
 
     }
     else {
+        for (int i =1; i< lista.size(); i = i+2) {
+            System.out.println(lista.get(i));
+        }
 
     }
 
-    return ;
+
 }
     public static void main(String[] args) {
 
         ArrayList <Integer> numberList = new ArrayList<>();
         numberList= makeList();
         System.out.println(numberList);
-        System.out.println("Ora ti inverto tutto, guarda:");
+        System.out.println("Ora ti stampo un array gaussiano, guarda:");
         System.out.println(invertList(numberList));
+        System.out.println("Ora ti stampo i numeri  in base alla loro posizione  pari o dispari nella prima lista ");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Inserisci 1 per indicare che vuoi le posizioni dispari, oppure 2 se le vuoi pari");
+        String input = scanner.nextLine();
+        int numero = Integer.parseInt(input);
+        boolean pari= true;
+        if (numero == 1){
+            pari = false;
 
+        }else {
+            pari= true;
+        }
+        halfList(numberList, pari);
 
 
     }
