b5f2943ab52f881e6a2f5d421313d43b02fa0396
Federico <brunetti.federico.13@gmail.com>
2025-06-24 17:12:42 +0200

    esercizio 2 completato

--- src/es2/Main.java
+++ src/es2/Main.java
@@ -1,7 +1,45 @@
 package es2;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+public static ArrayList makeList() {
+    Scanner scanner = new Scanner(System.in);
+    HashSet<String> wordSet = new HashSet<>();
+    System.out.println("Ciao, dimmi un numero intero ");
+    String input = scanner.nextLine();
+    int numero = Integer.parseInt(input);
+    ArrayList <Integer> numberList = new ArrayList<>(numero);
+    for (int i=0; i<numero; i++){
+        int casualNumber = (int) (Math.random() * 101);
+        numberList.add(casualNumber);
+
 
     }
+    return  numberList;
+}
+
+public static ArrayList invertList(ArrayList lista) {
+    ArrayList <Integer> oppositeList = new ArrayList<>();
+for (int i=lista.size()-1; i>=0; i--) {
+    oppositeList.add ((Integer) lista.get(i));
+
+}
+
+    return oppositeList;
+}
+    public static void main(String[] args) {
+
+        ArrayList <Integer> numberList = new ArrayList<>();
+        numberList= makeList();
+        System.out.println(numberList);
+        System.out.println("Ora ti inverto tutto, guarda:");
+        System.out.println(invertList(numberList));
+
+
+
+    }
+
 }
