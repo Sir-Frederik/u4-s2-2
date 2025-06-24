5dd7c922ba5472ddf253d8c87b8df5ba1473f070
Federico <brunetti.federico.13@gmail.com>
2025-06-24 16:38:09 +0200

    es1 completamente funzionante

--- src/es1/Main.java
+++ src/es1/Main.java
@@ -1,5 +1,6 @@
 package es1;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Scanner;
 
@@ -11,15 +12,31 @@ public class Main {
         System.out.println("Ciao, dimmi un numero intero ");
         String input = scanner.nextLine();
         int numero = Integer.parseInt(input);
+        ArrayList <String> wordList = new ArrayList<>(numero);
+        ArrayList <String> duplicateList = new ArrayList<>(numero);
         System.out.println(" Perfetto, ora inserisci "+numero + " parole");
         for (int i = 0; i <numero; i++){
             System.out.println("Inserisci la parola numero "+ (i+1)+ "/" +numero);
             String parola = scanner.nextLine();
             wordSet.add(parola);
+            if (wordList.contains(parola)) {
+                duplicateList.add(parola);
+                }
+
+            wordList.add(parola);
+
         }
-        System.out.println( "LE parole diverse che hai inserito sono: " + wordSet.size() + " ovvero:");
+        System.out.println( "Le parole univoche che hai inserito sono: " + wordSet.size() + " ovvero:");
 
         System.out.println(wordSet);
+        if (!duplicateList.isEmpty()){
+            System.out.println("Hai immesso anche delle parole duplicate e sono: ");
+            for (int i = 0; i < duplicateList.size(); i++){
+                System.out.println(duplicateList.get(i));
+
+            }
+
+        }
     }
 
 }
